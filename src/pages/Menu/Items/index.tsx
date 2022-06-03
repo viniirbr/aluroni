1302f4dfd7b2eaa0c7f9289de2c775378b25d5ad
Vinicius Ribeiro <vayne99@gmail.com>
2022-06-03 10:51:56 -0300

    Implement sorter

--- src/pages/Menu/Items/index.tsx
+++ src/pages/Menu/Items/index.tsx
@@ -10,15 +10,28 @@ interface Props {
 }
 
 export function Items({ search, filter, sorter }: Props) {
+
+    const sortByCategory = (category: string, list: Array<typeof menu[0]>) => {
+        switch(category) {
+            case 'porcao':
+                return list.sort((a,b) => a['size'] > b['size'] ? 1 : -1);
+            case 'qtd_pessoas':
+                return list.sort((a,b) => a['serving'] > b['serving'] ? 1 : -1);
+            case 'preco':
+                return list.sort((a,b) => a['price'] > b['price'] ? 1 : -1);
+        }
+        return list;
+    }
+
     return (
         <div className={styles.items}>
-            {menu
+            {sortByCategory(sorter.value, menu
             .filter((item) => item.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
             .filter((item) => {
                 if (filter) {
                     return item.category.id === filter;
                 } else return true;
-            })
+            }))
             .map((item) =>
                 <Item
                 {...item}/>)
